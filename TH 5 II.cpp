3a2d53bef522b5e07716427e587d41101df4c805
Tanphatta23tta <147957128+Tanphatta23tta@users.noreply.github.com>
2023-12-04 10:34:37 +0700

    Add files via upload

--- TH 5 II.cpp
+++ TH 5 II.cpp
@@ -4,25 +4,25 @@
 #include<ctype.h>
 #include<windows.h>
 char s1[50], s2[50], key;
+int a[100][100],i,j,n,m,x,tong;
 void Nhap();
-void Dem();
-void Ghep();
-void Dao();
+void Xuat();
+void sapxep();
+
+
 int main(){
 
 
 while(true)
 {
 system("cls");
-printf("************************************\n");
-printf("**       THAO TAC TREN CHUOI      **\n");
-printf("**   1. Nhap va chuan hoa chuoi   **\n");
-printf("**    2. Dem so tu trong chuoi    **\n");
-printf("**          3. Ghep chuoi         **\n");
-printf("**       4. Kiem tra doi xung     **\n");
-printf("**      5. In chuoi dao nguoc     **\n");
-printf("**            0. Thoat            **\n");
-printf("************************************\n");
+printf("***********************************************************************\n");
+printf("**                       THAO TAC TREN CHUOI                         **\n");
+printf("**       1. Nhap/xuat mang 2 chieu co m,n phan tu cac so nguyen      **\n");
+printf("**       2. Sap xep theo thu tu tang dan                             **\n");
+printf("**       3. Tinh tong cac phan tu la so nguyen to chia het cho 3     **\n");
+printf("**       0. Thoat                                                    **\n");
+printf("***********************************************************************\n");
 fflush(stdin);
 printf("\n \t\t AN PHIM CHON: ");
 scanf("%d",&key);
@@ -32,37 +32,29 @@ case 1:
 fflush(stdin);
 //goi ham de thuc hien cong viec tuong ung
 Nhap();
+printf("\n Mang vua nhap ");
+Xuat();
 printf("\nBam phim bat ky de tiep tuc!");
+
 
 
 getch();
 break;
 case 2:
 //goi ham thuc de hien cong viec tuong ung
-Dem();
+	printf("\n Sau khi sap xep ");
+
+sapxep();
+Xuat();
+
 printf("\nBam phim bat ky de tiep tuc!");
 
 getch();
 break;
-case 3:
-	Ghep();
-printf("\nBam phim bat ky de tiep tuc!");
-//goi ham thuc de hien cong viec tuong ung
 
-getch();
 
-break;
 
-case 4:
-printf("\nBam phim bat ky de tiep tuc!");
-//goi ham thuc de hien cong viec tuong ung
-break;
-case 5:
-	Dao();
-printf("\nBam phim bat ky de tiep tuc!");
-//goi ham thuc de hien cong viec tuong ung
-getch();
-break;
+
 case 0:
 exit(1);
 default:
@@ -75,28 +67,55 @@ break;
 }
 void Nhap()
 {
-	printf("\n Nhap I: ");
-	gets(s1);
-	printf("\n Nhap II: ");
-	gets(s2);
-	printf("\n Chuoi I %s",strupr(s1));
-	printf("\n Chuoi II %s",strupr(s2));
-}
-void Dem()
+	printf("\n Nhap m: ");
+	scanf("%d",&n);
+	printf("\n Nhap n: ");
+	scanf("%d",&m);
+	for (int i=0;i<n;i++)
+	for (int j=0;j<m;j++)
 	{
-		int lens1,lens2;
-		lens1=strlen(s1);
-		lens2=strlen(s2);
-		printf("\n Chuoi I co %d chu",lens1);
-		printf("\n Chuoi II co %d chu",lens2);
+		printf("\n nhap a[%d][%d]: ",i,j);
+		scanf("%d",&a[i][j]);
 	}
-void Ghep()
+	}
+	void Xuat()
 {
-	strcat(s1," ");
-	strcat(s1,s2);
-	printf("\n %s",s1);
+for(i=0;i<n;i++)
+  {
+  	printf("\n");
+  		for(j=0;j<m;j++)
+  
+  	printf("%d\t",a[i][j]);
+
+	
+  }
 }
-   void Dao()
-	{
-	printf("\n  %s ",strrev(s1));
+	
+void sapxep()
+{
+		int k= m*n;
+	for( int i=0;i<k-1;i++)
+	{ 
+	for( int j=i+1;j<k;j++)
+	  	{
+	
+	  		if(a[i/n][i%n]>a[j/n][j%n])
+	  		{
+	  				int temp=a[i/n][j%n];
+	       	   a[i/n][i%n]=a[j/n][j%n];
+	            	a[j/n][j/n]=temp;
+	  		}
+	  		
+	  	}
+    		
+	
+	
+		
 	}
+	 
+}
+
+
+
+
+
